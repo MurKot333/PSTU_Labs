1b74db3166d8615a8f944f3422e0b8aa25c9ce19
MurKot333 <63602064+MurKot333@users.noreply.github.com>
2025-10-15 14:08:55 +0500

    обновил task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,12 @@
+import math
+
+x = float(input("Введите значение переменной x: "))
+y = float(input("Введите значение переменной y: "))
+z = float(input("Введите значение переменной z: "))
+
+a = math.sqrt((x**3)/2) - math.sin(y)
+print(f"Получено значение функции a={a}")
+b = (math.exp(1)**2)/3 - math.cos(y) + z + math.log(y,math.exp(1))
+print(f"Получено значение функции b={b}")
+
 
